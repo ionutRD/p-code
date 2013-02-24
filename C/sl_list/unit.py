c6df3eb34b8c545d6f6f17f8d13164f53c061fa7
ihonius <ihonius@gmail.com>
2013-02-24 15:57:03 +0100

    Added unit test functionality for singly linked lists

--- C/sl_list/unit.py
+++ C/sl_list/unit.py
@@ -7,56 +7,40 @@ TEST_FILE_ERROR_MSG = "An error occurred while trying to \
 OS_ERROR_MSG = "An error occurred while trying to execute \
                 the following command: {0}"
 
+def unittest_eq(fin, assert_fun):
+  " Execute one unit test "
+  try:
+    test_file_name = fin
+    test_ref_name = "{0}.ref".format(test_file_name)
+    test_result_name = "{0}.result".format(test_file_name)
+    cmd = "./exec < {0} > {1}".format(test_file_name, \
+                                      test_result_name)
+    os.system(cmd)
+    test_result = open(test_result_name, "r")
+    test_ref = open(test_ref_name, "r")
+    result_content = test_result.readlines()
+    ref_content = test_ref.readlines()
+    assert_fun(result_content, ref_content)
+    test_result.close()
+    test_ref.close()
+    os.remove(test_result_name)
+  except IOError:
+    sys.stderr.write(TEST_FILE_ERROR_MSG.format(test_file_name,\
+                                                test_ref_name))
+  except OSError:
+    sys.stderr.write(OS_ERROR_MSG.format(cmd))
+
+
 class TestSimpleSinglyLinkedListOperations(unittest.TestCase):
-  def setUp(self):
-    self.in_content = None
-    self.ref_content = None
+  " Simple operations test suite "
 
   def test_add_back__1(self):
     " Test add_back 1 "
-    try:
-      test_file_name = "test/test1"
-      test_ref_name = "{0}.ref".format(test_file_name)
-      test_result_name = "{0}.result".format(test_file_name)
-      cmd = "./exec < {0} > {1}".format(test_file_name, \
-                                        test_result_name)
-      os.system(cmd)
-      test_result = open(test_result_name, "r")
-      test_ref = open(test_ref_name, "r")
-      self.result_content = test_result.readlines()
-      self.ref_content = test_ref.readlines()
-      self.assertEqual(self.result_content, self.ref_content)
-      test_result.close()
-      test_ref.close()
-      os.remove(test_result_name)
-    except IOError:
-      sys.stderr.write(TEST_FILE_ERROR_MSG.format(test_file_name,\
-                                                  test_ref_name))
-    except OSError:
-      sys.stderr.write(OS_ERROR_MSG.format(cmd))
+    unittest_eq("test/test1", self.assertEqual)
 
   def test_add_back__2(self):
     " Test add_back 2 "
-    try:
-      test_file_name = "test/test2"
-      test_ref_name = "{0}.ref".format(test_file_name)
-      test_result_name = "{0}.result".format(test_file_name)
-      cmd = "./exec < {0} > {1}".format(test_file_name, \
-                                        test_result_name)
-      os.system(cmd)
-      test_result = open(test_result_name, "r")
-      test_ref = open(test_ref_name, "r")
-      self.result_content = test_result.readlines()
-      self.ref_content = test_ref.readlines()
-      self.assertEqual(self.result_content, self.ref_content)
-      test_result.close()
-      test_ref.close()
-      os.remove(test_result_name)
-    except IOError:
-      sys.stderr.write(TEST_FILE_ERROR_MSG.format(test_file_name,\
-                                                  test_ref_name))
-    except OSError:
-      sys.stderr.write(OS_ERROR_MSG.format(cmd))
+    unittest_eq("test/test2", self.assertEqual)
 
 if __name__ == "__main__":
   suite = unittest.TestLoader().\
