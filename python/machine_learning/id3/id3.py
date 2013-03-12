ac12c78ad576112b1d0344ffce6a9ab7f94356a0
Ionut Radu <ihonius@gmail.com>
2013-03-12 19:52:34 +0200

    Changed id3.py

--- python/machine_learning/id3/id3.py
+++ python/machine_learning/id3/id3.py
@@ -3,41 +3,36 @@ import os
 import sys
 
 def entropy(data, attr):
-	""" 
-	Calculate entropy of a given data for the target attribute 
+	"""
+	Compute the entropy
 	"""
 	freq = {}
 	H = 0.0
 	
-	# Calculate the frequency of each value in the target attribute
 	for entry in data:
 		if freq.has_key(entry[attr]):
 			freq[entry[attr]] += 1.0
 		else:
 			freq[entry[attr]] = 1.0
-	
-	# Calculate the entropy of the data for the target attribute
+
 	for f in freq.values():
 		H += (-f / len(data)) * math.log(f / len(data), 2)
 	
 	return H
 
 def gain(data, attr, t_attr):
-	""" 
-	Calculates the information gain that would result by splitting the data
+	"""
+	Compute the gain
 	"""
 	freq = {}
 	S = 0.0
 	
-	# Calculate the frequency of each of the value in the target attribute
 	for entry in data:
 		if freq.has_key(entry[attr]):
 			freq[entry[attr]] += 1.0
 		else:
 			freq[entry[attr]] = 1.0
 	
-	# Calculate the sum of the entropy for each subset of records
-	# weighted by probability of occurring in the training set
 	for val in freq.keys():
 		prob = freq[val] / sum(freq.values())
 		subset = [rec for rec in data if rec[attr] == val]
@@ -72,7 +67,7 @@ def get_values(data, attr):
 def choose_attr(data, attrs, t_attr):
 	"""
     Cycles through all the attributes and returns the attribute with the
-    highest information gain (or lowest entropy).
+    highest information gain.
     """
 	data = data[:]
 	gain_dict = dict([(attr, gain(data, attr, t_attr)) \
@@ -98,34 +93,6 @@ def get_examples(data, attr, value):
 			ret_lst.extend(get_examples(data, attr, value))
 			return ret_lst
 
-def get_classification(entry, tree):
-	"""
-	Recursively traverses the decision tree
-	and returns a classification for the givel record
-	"""
-	
-	# If the current node is leaf
-	if type(tree) == type("string"):
-		return tree
-		
-	# Traverse until leaf node is found
-	else:
-		attr = tree.keys()[0]
-		t = tree[attr][entry[attr]]
-		return get_classification(entry, t)
-
-def classify(data, tree):
-	"""
-    Returns a list of classifications for each of the records in the data
-    list as determined by the given decision tree.
-    """
-	data = data[:]
-	classification = []
-	
-	for entry in data:
-		classification.append(get_classification(entry, tree))
-	
-	return classification
 
 def create_decision_tree(data, attrs, t_attr):
 	"""
@@ -142,24 +109,16 @@ def create_decision_tree(data, attrs, t_attr):
 		return vals[0]
 	
 	else:
-		# Choose the next best attribute to best classify our data
 		best = choose_attr(data, attrs, t_attr)
 
-		# Create a new decision tree/node with the best attribute and an empty
-		# dictionary object--we'll fill that up next.
 		tree = {best:{}}
 
-		# Create a new decision tree/sub-node for each of the values in the
-		# best attribute field
 		for val in get_values(data, best):
-			# Create a subtree for the current value under the "best" field
 			subtree = create_decision_tree(
 				get_examples(data, best, val), \
 				[attr for attr in attrs if attr != best], \
 				t_attr)
 
-			# Add the new subtree to the empty dictionary object in our new
-			# tree/node we just created.
 			tree[best][val] = subtree
 	
 	return tree
@@ -177,35 +136,22 @@ def get_input_file():
 	return fin
 
 def read_data(fin):
-    # Create a list of all the lines in the data file
     lines = [line.strip() for line in fin.readlines()]
 
-    # Remove the attributes from the list of lines and create a list of
-    # the attributes.
     lines.reverse()
     attrs = [attr.strip() for attr in lines.pop().split()]
     t_attr = attrs[-1]
     lines.reverse()
 
-    # Create a list of the data in the data file
     data = []
     for line in lines:
 		data.append(dict(zip(attrs,[datum.strip() \
 					for datum in line.split()])))
         
-    # Copy the data list into the examples list for testing
     examples = data[:]
     
-    # Create the decision tree
     tree = create_decision_tree(data, attrs, t_attr)
-
-    # Classify the records in the examples list
-    classification = classify(examples, tree)
-
-    # Print out the classification for each record
-    for item in classification:
-        print item
-
+	
     return tree
 
 def print_tree(tree, str):
@@ -219,15 +165,11 @@ def print_tree(tree, str):
 
 if __name__ == "__main__":
     fin = get_input_file()
-    print "------------------------\n"
-    print "--   Classification   --\n"
-    print "------------------------\n"
-    print "\n"    
     tree = read_data(fin)
     print "\n"
-    print "------------------------\n"
-    print "--   Decision Tree    --\n"
-    print "------------------------\n"
+    print "------------------------"
+    print "	    Decision Tree      "
+    print "------------------------"
     print "\n"
     print_tree(tree, "")
     fin.close()
--- python/machine_learning/mdp/mdp.py
+++ python/machine_learning/mdp/mdp.py
@@ -1,52 +1,35 @@
 import sys
 
-class MDP:
-	""" Markov Decision Proces
-		init_state = initial state
-		actions = set of actions
-		delta = used by algorithms
+class GridMDP:
+	""" 
+	A two dimension MDP.
 	"""
-	def __init__(self, init_state, actions_list, final_states, delta = .9):
-		""" Initialize the markov decision process 
-		"""
-		self.init_state = init_state
-		self.actions_list = actions_list
+	def __init__(self, grid, init_state = (0, 0), delta = .9):
+		self.init_state = (0, 0)
 		self.delta = delta
-		self.final_states = final_states
-		self.states = set()
-		self.reward = {}
-	
-	def T(self, state, action):
-		""" From a state and an action, 
-			return a list of pairs <probability, action>
-		"""
-		abstract
-	
-	def R(self, state):
-		""" Return a numeric reward for this state """
-		return self.reward[state]
-
-	def actions(self, state):
-		""" Return all possible actions given the current action """
-		abstract
-		
-class GridMDP(MDP):
-	""" A two dimension MDP.
-	"""
-	def __init__(self, grid, final_states, init_state = (0, 0), delta = .9):
-		MDP.__init__(self, init_state = init_state, \
-					 actions_list = [(0,1),(0,-1),(1,0),(-1,0)], \
-					 final_states = final_states, \
-					 delta = delta)
+		self.actions_list = [(0,1),(0,-1),(1,0),(-1,0)]
+		self.final_states = []
 		self.grid = grid
 		self.rows = len(grid)
 		self.cols = len(grid[0])
+		self.reward = {}
+		self.states = set()
 		for x in range(self.rows):
 			for y in range(self.cols):
 				self.reward[x, y] = grid[x][y]
 				self.states.add((x, y))
 	
+	def R(self, state):
+		""" 
+		Return a numeric reward for this state 
+		"""
+		return self.reward[state]
+	
 	def T(self, state, action):
+		""" 
+		From a state and an action, 
+		return a list of pairs <probability, action>
+		"""
 		i, j = state
 		if action == (-1, 0):
 			if j == 0:
@@ -69,9 +52,15 @@ class GridMDP(MDP):
 				return [(0.8, (i, j - 1)), (0.2, (i + 1, j))]
 	
 	def apply_state(self, state, action):
+		"""
+		Performa an action to a given state
+		"""
 		return (state[0] + action[0], state[1] + action[1])
 		
 	def actions(self, state):
+		""" 
+		Return all possible actions given the current action 
+		"""
 		if state in self.final_states:
 			return [None]
 		
@@ -92,42 +81,59 @@ class GridMDP(MDP):
 	
 
 def value_iteration(mdp, eps = 0.001):
-	""" Solving MDP by iteration """
+	""" 
+	Solving MDP by iteration 
+	"""
 	U1 = dict([(s, 0) for s in mdp.states])
 	R, T, delta = mdp.R, mdp.T, mdp.delta
 	while True:
 		U = U1.copy()
 		gamma = 0
 		for s in mdp.states:
-			U1[s] = R(s) + delta * max([sum([p * U[s1] for (p, s1) in T(s, a)]) for a in mdp.actions(s)])
+			U1[s] = R(s) + delta * max([sum([p * U[s1] \
+					for (p, s1) in T(s, a)]) for a in mdp.actions(s)])
 			gamma = max(gamma, abs(U1[s] - U[s]))
 		
-		if gamma < eps * (1 - delta) / delta:
+		if gamma < eps:
 			return U
 
-def argmin(seq, fn):
+def argmax(seq, fn):
+	"""
+	Return the maximum element from a list 
+	after applying fn for each element 
+	"""
 	best = seq[0]
 	best_score = fn(best)
 	for x in seq:
 		x_score = fn(x)
-		if  x_score < best_score:
+		if  x_score > best_score:
 			best, best_score = x, x_score
 	return best
-
-def argmax(seq, fn):
-	return argmin(seq, lambda x : -fn(x))
 			
 def best_policy(mdp, U):
-	""" Determine mdp best policy """
+	"""
+	Determine mdp best policy 
+	"""
+	
 	pi = {}
 	for s in mdp.states:
-		pi[s] = argmax(mdp.actions(s), lambda a : expected_utility(a, s, U, mdp))
+		pi[s] = argmax(mdp.actions(s), \
+					   lambda a : utility(a, s, U, mdp))
 	return pi
 
-def expected_utility(a, s, U, mdp):
+def utility(a, s, U, mdp):
+	""" 
+	Determine the expected utility in a state 
+	given an action and a utility
+	"""
+	
 	return sum([p * U[s1] for (p, s1) in mdp.T(s, a)])
 	
 def read_grid(file_name):
+	"""
+	Read the grid from the input file
+	"""
+	
 	try:
 		fhandle = open(file_name, "r")
 		grid = []
@@ -143,9 +149,18 @@ if __name__ == "__main__":
 	grid = read_grid(sys.argv[1])
 	rows = len(grid)
 	cols = len(grid[0])
-	mdp = GridMDP(grid, [])
+	mdp = GridMDP(grid)
 	U = value_iteration(mdp)
 	pi = best_policy(mdp, U)
-	print U
-	print "-----------------"
-	print pi
+	print "---------------------"
+	print "		UTILITY			"
+	print "---------------------"	
+	for value, key in U.items():
+		print value, ":", key
+	print "---------------------"
+	print "		BEST POLICY     "
+	print "---------------------"
+	pair_pi = pi.items()
+	pair_pi.sort()
+	for value, key in pair_pi:
+		print value, ":", key
