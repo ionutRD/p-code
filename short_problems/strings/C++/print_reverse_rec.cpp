9c05d23c5b5bc312fcf18747b68a3983eb239ca8
Ionut Radu <ihonius@gmail.com>
2013-11-19 00:19:39 +0200

    Solved print_reverse_rec in C++
    Signed-off-by: Ionut Radu <ihonius@gmail.com>

--- short_problems/strings/C++/print_reverse_rec.cpp
+++ short_problems/strings/C++/print_reverse_rec.cpp
@@ -9,7 +9,8 @@ using namespace std;
 void print_reverse_rec_aux(string::iterator & it, string::iterator & end)
 {
     if (it != end) {
-        print_reverse_rec_aux(it + 1, end);
+        auto it_next = it + 1;
+        print_reverse_rec_aux(it_next, end);
         cout << *it;
     }
 }
@@ -26,6 +27,10 @@ int main(int argc, char **argv)
     string s = "geeksforgeeks";
     cout << "Test 1: geeksforgeeks => " << endl;
     print_reverse_rec(s);
+    cout << endl;
+    string s2 = "ana are mere";
+    cout << "Test 1: ana are mere => " << endl;
+    print_reverse_rec(s2);
     cout << endl;
     return 0;
 }
