8111ada9301fc84edd49f9028072e2ecc428b20f
Ionut Radu <ihonius@gmail.com>
2014-07-27 15:32:59 +0300

    Changed builtin array to c++11 array
    Signed-off-by: Ionut Radu <ihonius@gmail.com>

--- short_problems/C++/first_non_repeating.cpp
+++ short_problems/C++/first_non_repeating.cpp
@@ -1,8 +1,8 @@
 /**
  * Find the first non-repeating character from a string.
  */
+#include <array>
 #include <iostream>
-#include <algorithm>
 #include <climits>
 #include <stdexcept>
 #include <string>
@@ -16,21 +16,21 @@ char find_first_non_repeating(const string &str)
     if (str.size() == 0) {
         throw invalid_argument("The string cannot be empty");
     }
-    char char_pos[NO_CHARS] = { 0 };
-    fill_n(char_pos, NO_CHARS, -1);
+    array<int, NO_CHARS> char_pos;
+    char_pos.fill(-1);
     for (int i = 0; i < str.size(); i++) {
-        if (char_pos[str.at(i)] == -1) {
-            char_pos[str.at(i)] = i;
-        } else if (char_pos[str.at(i)] >= 0) {
-            char_pos[str.at(i)] = -2;
+        if (char_pos.at(str.at(i)) == -1) {
+            char_pos.at(str.at(i)) = i;
+        } else if (char_pos.at(str.at(i)) >= 0) {
+            char_pos.at(str.at(i)) = -2;
         }
     }
 
     char first_non_repeating;
     int min_index = INT_MAX;
     for (int i = 0; i < NO_CHARS; i++) {
-        if (char_pos[i] >= 0 && min_index > char_pos[i]) {
-            min_index = char_pos[i];
+        if (char_pos.at(i) >= 0 && min_index > char_pos.at(i)) {
+            min_index = char_pos.at(i);
             first_non_repeating = static_cast<char>(i);
         }
     }
