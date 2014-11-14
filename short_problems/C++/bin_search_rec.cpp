655275c7a3867c051c02ce4b4d8abda2693e1530
Ionut Radu <ihonius@gmail.com>
2014-11-14 23:04:49 +0200

    Solve binary search recursively in C++
    Signed-off-by: Ionut Radu <ihonius@gmail.com>

--- short_problems/C++/bin_search_rec.cpp
+++ short_problems/C++/bin_search_rec.cpp
@@ -1,33 +1,37 @@
+#include <functional>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-template <typename InputIt, typename T, typename Comp>
-bool binary_search(InputIt first, InputIt last, const T &key)
+template <typename InputIt, typename T, typename Compare = less<T>>
+bool bin_search(InputIt first, InputIt last, const T &key, Compare comp = Compare())
 {
-    if (first == last) {
+    if (first == last || first + 1 == last) {
         return false;
     }
     auto middle = first + (last - first) / 2;
-    if (Comp(key, *middle) == 0) {
+    if (key == *middle) {
         return true;
-    } else if (Comp(key, *middle) < 0) {
-        return binary_search(first, middle, key);
-    } else return binary_search(middle, last, key);
+    } else if (comp(key, *middle)) {
+        return bin_search(first, middle, key);
+    } else return bin_search(middle, last, key);
 }
 
 int main()
 {
     vector<int> coll1 {1, 5, 7, 11, 12};
     int key1 = 5;
-    cout << binary_search(begin(coll1), end(coll1), key1) << endl;
+    cout << bin_search(begin(coll1), end(coll1), key1) << endl;
     int key2 = 4;
-    cout << binary_search(begin(coll1), end(coll1), key2) << endl;
+    cout << bin_search(begin(coll1), end(coll1), key2) << endl;
     int key3 = 12;
-    cout << binary_search(begin(coll1), end(coll1), key3) << endl;
+    cout << bin_search(begin(coll1), end(coll1), key3) << endl;
     vector<int> coll2 {1, 5, 7, 11, 12, 15};
     int key4 = 6;
-    cout << binary_search(begin(coll2), end(coll2), key4) << endl;
+    cout << bin_search(begin(coll2), end(coll2), key4) << endl;
+    vector<int> coll3 {};
+    int key5 = 5;
+    cout << bin_search(begin(coll3), end(coll3), key5) << endl;
     return 0;
 }
